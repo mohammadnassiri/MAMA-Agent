8b058a39640c8f70329b4e73b555ace013951bfb
Mohammad <mohammadnassiri>
2018-03-25 10:03:04 +0430

    send file request.

--- agent.py
+++ agent.py
@@ -2,10 +2,33 @@ import os
 import subprocess
 import shlex
 import threading
-
+import urllib
 import pefile
 import requests
 import time
+
+
+class Server:
+
+    # author: https://gist.github.com/Integralist/3f004c3594bbf8431c15ed6db15809ae
+
+    def __init__(self, url):
+        self.url = url
+
+    def request(self):
+        response = None
+        r = requests.post(self.url, data={'arch': "IMAGE_FILE_MACHINE_I386"})  # IMAGE_FILE_MACHINE_I386 is x86
+        if r.status_code == 200:
+            response = r
+        return response
+
+    def download(self, id, name):
+        status = False
+        url = self.url + '/download'
+        file = urllib.URLopener()
+        if file.retrieve(url, name):
+            status = True
+        return status
 
 
 class Executor:
@@ -45,22 +68,6 @@ class Executor:
         return self.process.returncode, self.stdout, self.stderr
 
 
-class Server:
-
-    # author: https://gist.github.com/Integralist/3f004c3594bbf8431c15ed6db15809ae
-
-    def __init__(self, url):
-        self.url = url
-
-    def connect(self):
-        return requests.get(self.url)
-
-    def download(self, sub_url, name, file_dir):
-        url = self.url + '/' + sub_url + '/' + name
-        r = requests.get(url)
-        open(os.path.join(file_dir, name), 'wb').write(r.content)
-
-
 class PEInfo:
     # author: https://github.com/nheijmans/malzoo/blob/master/malzoo/modules/tools/pe.py
     def __init__(self, filename):
@@ -84,17 +91,18 @@ class PEInfo:
 
 
 class Trace:
-    def __init__(self, pin_path, wao_path, file_path, timeout=None):
+    def __init__(self, pin_path, wao_path, file_path, arch, timeout=None):
         self.pin_path = pin_path
         self.wao_path = wao_path
         self.file_path = file_path
+        self.arch = arch
         self.timeout = timeout
 
     def wao(self):
         return
 
     def pintool(self):
-        if PEInfo(self.file_path).get_cpu_type() is "IMAGE_FILE_MACHINE_I386":
+        if self.arch is "IMAGE_FILE_MACHINE_I386":
             print(self.pin_path + "ia32/bin/pin -t " + self.pin_path + "source/tools/godware/obj-ia32/godware.dll -- " + self.file_path + " " + self.pin_path + "source/tools/godware/msgbox.exe")
             x, y, z = Executor(
                 self.pin_path + "ia32/bin/pin -t " + self.pin_path + "source/tools/godware/obj-ia32/godware.dll -- " + self.file_path + " " + self.pin_path + "source/tools/godware/msgbox.exe",
@@ -111,25 +119,30 @@ class Trace:
 if __name__ == '__main__':
 
     the_end = 120
-    response = 0
+    server = Server('http://localhost:8000/api/')
+
     # wait for 120 seconds to if a new file uploaded. it must be infinite loop in production
     while the_end > 0:
-        response = Server('http://localhost:8000/api/').connect().json()
-        print(response)
-        if response is not 0:
+        response = server.request().json()
+        if response is not None:
             break
         the_end -= 1
         time.sleep(1)
+
+    file_id = response['id']
+    file_name = response['name']
+    file_arch = response['arch']
 
     # set path of two tools and the file that will be traced
     pin_path = "C:/Users/MA/Desktop/work/api-seq-tools/pin-2.14-71313-msvc9-windows/"
     file_path = pin_path + "/source/tools/godware/poc2.exe"
 
     # we will first check the runpe for all samples then in second phase we will trace with wao
-    # trace file with pintool. then if it used runpe
-    if response == 0:
-        Trace(pin_path, "", file_path).pintool()
+    trace = Trace(pin_path, "", file_path, 120)
+
+    # trace file with pintool for runpe
+    #trace.pintool()
 
     # trace file with wao. there must be only one option wao or pin. the vm must revert before other test.
-    Trace(pin_path, "", file_path).wao()
+    #trace.wao()
 
